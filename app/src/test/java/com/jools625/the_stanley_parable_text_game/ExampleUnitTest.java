d21759a47321f13630b2b52ae9b318ac02721dc7
Jools <jools.hg@gmail.com>
2019-11-27 16:35:12 +0000

    Initial commit

---
+++ app/src/test/java/com/jools625/the_stanley_parable_text_game/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.jools625.the_stanley_parable_text_game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
