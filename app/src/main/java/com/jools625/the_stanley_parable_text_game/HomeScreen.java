20cbfda6dd6cabe8fd52fa68db6b35eb8a96bb5b
Jools <jools.hg@gmail.com>
2019-11-27 16:52:46 +0000

    created basic home screen

--- app/src/main/java/com/jools625/the_stanley_parable_text_game/HomeScreen.java
+++ app/src/main/java/com/jools625/the_stanley_parable_text_game/HomeScreen.java
@@ -2,13 +2,27 @@ package com.jools625.the_stanley_parable_text_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
+
+    Button btnStartGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStartGame = findViewById(R.id.buttonStartGame);
+
+        btnStartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomeScreen.this, Intro.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/jools625/the_stanley_parable_text_game/Intro.java
@@ -0,0 +1,14 @@
+package com.jools625.the_stanley_parable_text_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
